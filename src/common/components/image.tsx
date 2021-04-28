ef3ac43dde4655097f3694b6006ff0408659d088
Calvin Ling <calvinling8@gmail.com>
2021-04-27 21:42:19 -0400

    Do not show a placeholder when an image is ready

--- src/common/components/image.tsx
+++ src/common/components/image.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 import { Skeleton, SkeletonProps } from "@material-ui/lab";
-import React, { ImgHTMLAttributes, useState } from "react";
+import { ImgHTMLAttributes, useLayoutEffect, useRef, useState } from "react";
 
 interface ImageProps extends ImgHTMLAttributes<HTMLImageElement> {
   skeletonProps?: SkeletonProps;
@@ -13,7 +13,21 @@ export default function Image({
   className,
   ...imageProps
 }: ImageProps) {
+  const imageRef = useRef<HTMLImageElement>(null);
   const [loaded, setLoaded] = useState(false);
+
+  useLayoutEffect(() => {
+    // Do not show the placeholder if the image has already completely loaded
+    if (imageRef.current === null) {
+      return;
+    }
+    if (imageRef.current.complete) {
+      setLoaded(true);
+    } else {
+      imageRef.current.onload = () => setLoaded(true);
+      imageRef.current.onerror = () => setLoaded(true);
+    }
+  }, []);
 
   return (
     <>
@@ -21,9 +35,9 @@ export default function Image({
         <Skeleton className={className} variant="rect" {...skeletonProps} />
       )}
       <img
+        ref={imageRef}
         className={className}
         style={{ display: loaded ? undefined : "none" }}
-        onLoad={() => setLoaded(true)}
         {...imageProps}
       />
     </>
